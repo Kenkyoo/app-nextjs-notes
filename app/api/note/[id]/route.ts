e278fa7d7f9e35a07cda830d8082d08702f48419
Franco <frfantusatti@protonmail.com>
2025-04-26 07:23:31 -0300

    finish debug

--- app/api/note/[id]/route.ts
+++ app/api/note/[id]/route.ts
@@ -1,26 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/db";
 
-export async function PATCH(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  const { title, content, color } = await req.json();
-
-  await prisma.note.update({
-    where: {
-      id: params.id,
-    },
-    data: {
-      title,
-      content,
-      color,
-    },
-  });
-
-  return NextResponse.json({ message: "Updated" }, { status: 200 });
-}
-
 export async function DELETE(req: Request) {
   const { id } = await req.json();
 
